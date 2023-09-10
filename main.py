8c4f182fa8a90037832091c9b15b87183c945a2f
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:45:07 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import time
 class WebScraper:
     def __init__(self, url):
         self.url = url
-    
+
     def scrape(self):
         response = requests.get(self.url)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -26,14 +26,14 @@ class WebScraper:
         text = [p.get_text() for p in paragraphs]
         image_urls = [img['src'] for img in images]
         link_urls = [link['href'] for link in links]
-        
+
         extracted_info = {
             'title': title,
             'text': text,
             'images': image_urls,
             'links': link_urls
         }
-        
+
         return extracted_info
 
 
@@ -41,11 +41,11 @@ class GoogleSearch:
     def __init__(self, api_key, engine_id):
         self.api_key = api_key
         self.engine_id = engine_id
-        
+
     def search(self, query, num_results):
         service = build("customsearch", "v1", developerKey=self.api_key)
         result = service.cse().list(q=query, cx=self.engine_id, num=num_results).execute()
-        
+
         return result['items']
 
 
@@ -67,7 +67,7 @@ class SentimentAnalyzer:
             sentiment_label = 'negative'
         else:
             sentiment_label = 'neutral'
-        
+
         return sentiment_label
 
 
@@ -94,13 +94,13 @@ class WordCloudCreator:
 class RecommendationEngine:
     def __init__(self, user_input):
         self.user_input = user_input
-    
+
     def generate_recommendations(self, analysis_results):
         recommendations = []
-        
+
         # Implement your personalized recommendation logic here
         # You can use user input and analysis results to generate recommendations
-        
+
         return recommendations
 
 
@@ -109,44 +109,46 @@ class RealTimeUpdater:
         self.url = url
         self.refresh_rate = refresh_rate
         self.user_input = user_input
-    
+
     def run(self):
         web_scraper = WebScraper(self.url)
         data_cleaner = DataCleaner()
         sentiment_analyzer = SentimentAnalyzer()
         trend_analyzer = TrendAnalyzer()
         recommendation_engine = RecommendationEngine(self.user_input)
-        
+
         while True:
             extracted_info = web_scraper.scrape()
-            cleaned_text = data_cleaner.clean(' '.join(extracted_info['text']))  # Convert list of paragraphs to a single string
+            # Convert list of paragraphs to a single string
+            cleaned_text = data_cleaner.clean(' '.join(extracted_info['text']))
             sentiment_label = sentiment_analyzer.analyze(cleaned_text)
             trends = trend_analyzer.analyze(cleaned_text)
-            
+
             analysis_results = {
                 'sentiment_label': sentiment_label,
                 'trends': trends
             }
-            
-            recommendations = recommendation_engine.generate_recommendations(analysis_results)
-            
+
+            recommendations = recommendation_engine.generate_recommendations(
+                analysis_results)
+
             # Print or display the analysis results and recommendations
             print("Sentiment Label:", sentiment_label)
             print("Trends:", trends)
             print("Recommendations:", recommendations)
-            
+
             time.sleep(self.refresh_rate)
 
 
 if __name__ == '__main__':
     # Example usage
-    
+
     # Web scraping
     url = 'https://www.example.com'
     web_scraper = WebScraper(url)
     extracted_info = web_scraper.scrape()
     print(extracted_info)
-    
+
     # Google search
     api_key = 'YOUR_API_KEY'
     engine_id = 'YOUR_ENGINE_ID'
@@ -155,28 +157,28 @@ if __name__ == '__main__':
     num_results = 10
     search_results = google_search.search(query, num_results)
     print(search_results)
-    
+
     # Data cleaning
     text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     data_cleaner = DataCleaner()
     cleaned_text = data_cleaner.clean(text)
     print(cleaned_text)
-    
+
     # Sentiment analysis
     sentiment_analyzer = SentimentAnalyzer()
     sentiment_label = sentiment_analyzer.analyze(text)
     print(sentiment_label)
-    
+
     # Trend analysis
     trend_analyzer = TrendAnalyzer()
     trends = trend_analyzer.analyze(text)
     print(trends)
-    
+
     # Word cloud
     wordcloud_creator = WordCloudCreator()
     data = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     wordcloud_creator.create(data)
-    
+
     # Personalized recommendations
     user_input = 'I want to learn data science.'
     recommendation_engine = RecommendationEngine(user_input)
@@ -184,9 +186,10 @@ if __name__ == '__main__':
         'sentiment_label': sentiment_label,
         'trends': trends
     }
-    recommendations = recommendation_engine.generate_recommendations(analysis_results)
+    recommendations = recommendation_engine.generate_recommendations(
+        analysis_results)
     print(recommendations)
-    
+
     # Real-time updates
     url = 'https://www.example.com'
     refresh_rate = 60  # 1 minute
